f5374ca1212ea3c68857eccd354a4e44693ac5bc
danielrubens <danielrubensdaniel@gmail.com>
2022-12-04 23:31:37 -0300

    SectionText

--- src/components/Hero/Hero.js
+++ src/components/Hero/Hero.js
@@ -5,9 +5,17 @@ import Button from '../../styles/GlobalComponents/Button';
 import { LeftSection } from './HeroStyles';
 
 const Hero = (props) => (
-  <div>
-    Hero
-  </div>
+  <Section row nopadding>
+    <LeftSection>
+      <SectionTitle main center>
+          Welcome To <br />
+          My Personal Portfolio
+      </SectionTitle>
+      <SectionText>
+        Here you have some of my projects, approaching Python Bots, Geographic Information System, Node and React.
+      </SectionText>
+    </LeftSection>
+  </Section>
 );
 
 export default Hero;
