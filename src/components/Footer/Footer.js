7408a15648632463f1c38511c104255dd051bce9
danielrubens <danielrubensdaniel@gmail.com>
2022-12-05 02:16:22 -0300

    FooterWrapper

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -6,9 +6,29 @@ import { CompanyContainer, FooterWrapper, LinkColumn, LinkItem, LinkList, LinkTi
 
 const Footer = () => {
   return (
-    <div>
-      Footer
-    </div>
+    <FooterWrapper>
+     <SocialContainer>
+    <LinkList>
+      <LinkColumn>
+      <LinkTitle>Call</LinkTitle>
+      <LinkItem href="tel:5585981596937">+55 (85) 9-8159-6937</LinkItem>
+      </LinkColumn>
+      <LinkColumn>
+      <LinkTitle>Email</LinkTitle>
+      <LinkItem href="mailto:contact@danielrubens.com">contact@danielrubens.com</LinkItem>
+      </LinkColumn>
+    </LinkList>
+      <SocialIcons href='https://github.com/danielrubens'>
+        <AiFillGithub size='3rem'/>
+      </SocialIcons>
+      <SocialIcons href='https://www.linkedin.com/in/danielrubenss/'>
+        <AiFillLinkedin size='3rem'/>
+      </SocialIcons>
+      <SocialIcons href='https://instagram.com/danielrubenss'>
+        <AiFillInstagram size='3rem'/>
+      </SocialIcons>
+      </SocialContainer>
+   </FooterWrapper>
   );
 };
 
