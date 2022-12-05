15b6d73b8ff7608c24c9be8d7e9a1e9869057d84
danielrubens <danielrubensdaniel@gmail.com>
2022-12-05 00:06:27 -0300

    Techonologies component: ListContainer

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -10,6 +10,17 @@ const Technologies = () =>  (
     <SectionText>
       I've worked with MERN Stack, GIS Development, Python Bots and Clean Architecture.
     </SectionText>
+    <List>
+      <ListItem>
+        <DiFirebase size='3rem'/>
+        <ListContainer>
+          <ListTitle>Front-end</ListTitle>
+          <ListParagraph>Experience with <br />
+          React.js
+          </ListParagraph>
+        </ListContainer>
+      </ListItem>
+    </List>
   </Section>
 );
 
