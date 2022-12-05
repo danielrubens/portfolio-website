c2365d310a93722e6dc0e88e7f43d186be9280d2
danielrubens <danielrubensdaniel@gmail.com>
2022-12-04 23:56:39 -0300

    Tags mapped

--- src/components/Projects/Projects.js
+++ src/components/Projects/Projects.js
@@ -18,6 +18,14 @@ const Projects = () => (
             <Hr />
           </TitleContent>
           <CardInfo>{description}</CardInfo>
+          <div>
+            <TitleContent>Stack</TitleContent>
+            <TagList>
+              {tags.map((tag, i) => (
+                <Tag key={i}>{tag}</Tag>
+              ))}
+            </TagList>
+          </div>
         </BlogCard>
       ))}
     </GridContainer>
