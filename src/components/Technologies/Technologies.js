dae617f1c6f235d078436b543971ff39fe487728
danielrubens <danielrubensdaniel@gmail.com>
2022-12-05 00:03:48 -0300

    SectionDivider

--- src/components/Technologies/Technologies.js
+++ src/components/Technologies/Technologies.js
@@ -4,9 +4,13 @@ import { Section, SectionDivider, SectionText, SectionTitle } from '../../styles
 import { List, ListContainer, ListItem, ListParagraph, ListTitle } from './TechnologiesStyles';
 
 const Technologies = () =>  (
-  <div>
-    Technologies
-  </div>
+  <Section id='tech'>
+    <SectionDivider />
+    <SectionTitle>Techonologies</SectionTitle>
+    <SectionText>
+      I've worked with MERN Stack, GIS Development, Python Bots and Clean Architecture.
+    </SectionText>
+  </Section>
 );
 
 export default Technologies;
